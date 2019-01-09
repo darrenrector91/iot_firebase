f7bb3cd49b0e6505c61723d3193a38513db71e65
darrenrector91 <darrenrector91@gmail.com>
2019-01-09 01:48:38 -0600

    moved temp gauge out of testing

---
+++ server/public/js/temp-gauge.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+
+    const records = 1;
+
+    firebase.database().ref('temp_timestamped').limitToLast(records).on('value', ts_measures => {
+        // let values = [];
+
+        ts_measures.forEach(ts_measure => {
+            temp = ts_measure.val().value;
+            // values.push(ts_measure.val().value);
+        });
+
+        google.charts.load('current', { 'packages': ['gauge'] });
+        google.charts.setOnLoadCallback(drawChart);
+
+        function drawChart() {
+
+            var mydata = google.visualization.arrayToDataTable([
+                ['Label', 'Value'],
+                ['˚F', temp],
+            ]);
+
+            var myoptions = {
+                width: 500, height: 300,
+                redFrom: 90, redTo: 100,
+                yellowFrom: 80, yellowTo: 90,
+                greenFrom: 0,
+                greenTo: 32,
+                greenColor: "#399cbd",
+                minorTicks: 5,
+                majorTicks: ['0', '', '20', '', '40', '', '60', '', '80', '', '100']
+            };
+
+            var chart = new google.visualization.Gauge(document.getElementById('chart_div'));
+
+            chart.draw(mydata, myoptions);
+
+        }
+    });
+});
